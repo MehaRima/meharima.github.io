f69161a24cd7166f9642037b168d59c96fb27c1c
SAHELI  BASU <26185528+MehaRima@users.noreply.github.com>
2020-11-29 21:41:59 +0530

    Update answer.php

--- answer.php
+++ answer.php
@@ -1,11 +1,6 @@
 <html>
 <body>
 
-<form  method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
 Thanks For your Message!! 
 <?php echo $_GET["name"]; ?><br>  <?php echo $_GET["name"]; ?>
 
